d618aab5434ef86c6005a908a86e30ac0f24644f
rafalmeidas <rafalmeidas21@gmail.com>
2026-06-23 08:53:50 -0300

    chore: remove arquivos de teste do scaffold Quarkus

--- src/test/java/com/pdi/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.pdi;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends ProductResourceTest {
-    // Executa os mesmos testes de ProductResourceTest no modo packaged (JAR/native).
-}
--- src/test/java/com/pdi/GreetingResourceTest.java
+++
@@ -1,8 +0,0 @@
-package com.pdi;
-
-import io.quarkus.test.junit.QuarkusTest;
-
-@QuarkusTest
-class GreetingResourceTest {
-    // placeholder mantido para compatibilidade com GreetingResourceIT
-}
